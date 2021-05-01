df9d490f0af871d86ff9d7f435a3bc43777bff07
burtk33 <burtk33@gmail.com>
2021-05-01 15:34:15 -0400

    adding IndexedDB file

--- public/db.js
+++ public/db.js
@@ -1,3 +1,4 @@
+
 let db;
 const request = indexedDB.open("budget", 1);
 
@@ -8,3 +9,47 @@ request.onupgradeneeded = (event) => {
     autoIncrement: true 
   });
 };
+
+request.onsuccess = (event) => {
+  db = event.target.result;
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = (event) => {
+  console.log("Error: " + event.target.errorCode);
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction("pending", "readwrite");
+  const store = transaction.objectStore("pending");
+  store.add(record);
+}
+
+function checkDatabase() {
+  const transaction = db.transaction("pending", "readwrite");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = () => {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => response.json())
+      .then(() => {
+        const transaction = db.transaction("pending", "readwrite");
+        const store = transaction.objectStore("pending");
+        store.clear();
+      });
+    }
+  };
+}
+
+window.addEventListener("online", checkDatabase);
